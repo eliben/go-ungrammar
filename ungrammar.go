faa332e6c80a3a848a8a32dabdc46765d071ca1b
Eli Bendersky <eliben@gmail.com>
2023-06-17 06:36:31 -0700

    Start with Rule interface and concrete types

---
+++ ungrammar.go
@@ -0,0 +1,43 @@
+package ungrammar
+
+type Rule interface {
+	isRule()
+}
+
+type Labeled struct {
+	Label string
+	Rule  Rule
+}
+
+type Node struct {
+	Name string
+	Rule Rule
+}
+
+type Token struct {
+	Value string
+}
+
+type Seq struct {
+	Rules []Rule
+}
+
+type Alt struct {
+	Rules []Rule
+}
+
+type Opt struct {
+	Rule Rule
+}
+
+type Rep struct {
+	Rule Rule
+}
+
+func (_ Labeled) isRule()
+func (_ Node) isRule()
+func (_ Token) isRule()
+func (_ Seq) isRule()
+func (_ Alt) isRule()
+func (_ Opt) isRule()
+func (_ Rep) isRule()
